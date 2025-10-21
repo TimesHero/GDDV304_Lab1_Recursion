5d06ade2de36ecac063f14b9ba5a9056532f7fc7
TimesHero <121903190+TimesHero@users.noreply.github.com>
2025-10-20 20:19:16 -0400

    I think it's finished? And it was finished and submitted a while go. Oops!

--- Assets/Scripts/RecursiveFunctions.cs
+++ Assets/Scripts/RecursiveFunctions.cs
@@ -7,38 +7,53 @@ public class RecursiveFunctions : MonoBehaviour
 
     void Start()
     {
-        Debug.Log(FibonacciSequence(fibonacciNumber));
-        Debug.Log(CountAllChildren(transform));
-        Debug.Log(Factorial(factorialNumber));
+        Debug.Log($"Fibonacci: {FibonacciSequence(fibonacciNumber)}.");
+        Debug.Log($"{CountAllChildren(transform)} children.");
+        Debug.Log($"Factorial: {Factorial(factorialNumber)}.");
     }
 
     private int FibonacciSequence(int n)
     {
         // If n is less than 2, then return n.
+        if (n < 2)
+        {
+            return n;
+        }
         // Return the result of FibonacciSequence of n - 1 plus the result of FibonacciSequence of n - 2.
-
-        return -1; // ← remove this
+        else
+        {
+            return FibonacciSequence(n-1) + FibonacciSequence(n-2);
+        }
     }
 
     private int CountAllChildren(Transform parent)
     {
         // Declare a new integer called total and set it to 0.
-
+        int total = 0;
         // Iterate through the children of parent
-        // Add to total 1 plus the result of counting current child's children.
-
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+            total += 1;
+            total += CountAllChildren(child);
+        }
         // return total
+        return total;
 
-        return -1; // ← remove this
     }
 
     private int Factorial(int n)
     {
         // if n is less than or equal to 1
         // return 1
-
+        if (n <= 1)
+        {
+            return 1;
+        }
         // return n multiplied but the result of n-1! (that is, the factorial of n-1).
-        
-        return -1; // ← remove this
+        else
+        {
+            return n * Factorial(n - 1);
+        }
     }
 }
